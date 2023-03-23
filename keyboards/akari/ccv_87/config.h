9a0ff4aeaf9edcd71f804cec198bba35138df6d6
Akari <38679018+dr-Akari@users.noreply.github.com>
2023-03-24 03:18:52 +0530

    First Compiled - Added Encoder, Trackball and OLED w/ TKL split config _ccv_87

--- keyboards/akari/ccv_87/config.h
+++ keyboards/akari/ccv_87/config.h
@@ -7,14 +7,12 @@
 
 #define DEBOUNCE 5
 
-// #define POINTING_DEVICE_ROTATION_0
-#define TRACKBALL_RIGHT
+// set which MCU has the pointing device attached
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_ROTATION_180
+#define POINTING_DEVICE_SCALE 1
 
-#ifdef TRACKBALL_RIGHT
-  #define POINTING_DEVICE_INVERT_X
-  #define POINTING_DEVICE_INVERT_Y
-#endif
-
+#define LAYER_STATE_8BIT // limiting the number of layers since we have under 4 layers.
 
 
 // left encoder absent. Only right encoder at B7 and C7 on puchi C
@@ -26,7 +24,7 @@
 #define ENCODERS_RESOLUTIONS_RIGHT { 4 }
 #define ENCODER_MAP_KEY_DELAY 10
 
-
+// #define WPM_ALLOW_COUNT_REGRESSION
 
 // OLED I2C 128x64px _Right keyboard
 #ifdef OLED_ENABLE
@@ -34,19 +32,23 @@
   #define OLED_DISPLAY_HEIGHT 64
   #define OLED_DISPLAY_WIDTH 128
   #define OLED_DISPLAY_128X64
-  #define OLED_TIMEOUT 30000
+  #define OLED_TIMEOUT 0
 #endif
 
-
-// Split Config
-//#define USE_SERIAL // Use serial for communication between boards. bit banged 3 wire. Rx-->Rx, GND, VCC
-//#define SOFT_SERIAL_PIN D2 // D2 pin on puchi-C. i.e., RX
 
 #define SPLIT_LAYER_STATE_ENABLE // This enables syncing of the layer state between both halves of the split keyboard. The main purpose of this feature is to enable support for use of things like OLED display of the currently active layer.
 #define SPLIT_WPM_ENABLE // This enables transmitting the current WPM to the slave side of the split keyboard. The purpose of this feature is to support cosmetic use of WPM (e.g., displaying current value on an OLED screen)
 #define SPLIT_OLED_ENABLE // Enables transmitting the current OLED on/off status to the slave side of the split keyboard. The purpose of this feature is to support state (on/off state only) syncing.
 #define SPLIT_POINTING_ENABLE // Enables transmitting the pointing device status to the master side of the split keyboard.
 #define EE_HANDS // EEPROM HANDEDNESS
+
+// bootmagic config
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW_RIGHT 6
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 8
+
+
 
 // #define LAYER_STATE_8BIT
 /*
--- keyboards/akari/ccv_87/rules.mk
+++ keyboards/akari/ccv_87/rules.mk
@@ -20,15 +20,20 @@ POINTING_DEVICE_ENABLE = yes # Enable Pimoroni Trackball
 POINTING_DEVICE_DRIVER = pimoroni_trackball
 
 ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
+ENCODER_MAP_ENABLE = yes # i want to subsequently enable encoder as well
 
 
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = yes
+SPACE_CADET_ENABLE = no           # Enable N-Key Rollover
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
+
 
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
